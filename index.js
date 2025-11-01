4c98f8e3e2180d3362282af541efbc876080e03e
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-11-01 03:13:23 +0100

    Rewrite index.js in pure JS instead of jQuery

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function isFieldEmpty(fieldContent) {
-  return fieldContent.trim().length === 0;
-}
-
-function addNotification($input) {
-  var inputName = $input.attr("name");
+/* error element creator */
+function addNotification(input) {
+  var inputName = input.name;
   var fieldName;
 
   switch(inputName) {
@@ -26,41 +23,62 @@ function addNotification($input) {
   return `${fieldName} cannot be empty`;
 }
 
-function showEmptyFieldError( $input) {
-  $input.addClass("empty-signup-input");
-  $input.after(`<p class="empty-signup-notification">${addNotification($input)}</p>`);
+function createErrorElement(errorMessage) {
+  var errorElement = document.createElement("p");
+  errorElement.className = "empty-signup-notification";
+  errorElement.textContent = errorMessage;
+
+  return errorElement;
+}
+/* ------------------------------------------------------- */
+
+function isFieldEmpty(fieldContent) {
+  return fieldContent.trim().length === 0;
 }
 
-function showInvalidEmailError($input) {
-  $input.attr("placeholder", $input.val());
-  $input.addClass("empty-signup-input");
-  $input.after(`<p class="empty-signup-notification">Looks like this is not an email</p>`);
+function clearInputError(input) {
+  input.classList.remove("empty-signup-input");
+
+  if (input.nextElementSibling && input.nextElementSibling.classList.contains("empty-signup-notification")) 
+    input.nextElementSibling.remove();
 }
 
-function clearInputError($input) {
-  $input.removeClass("empty-signup-input");
-  $input.next("p.empty-signup-notification").remove();
+function showEmptyFieldError(input) {
+  input.classList.add("empty-signup-input");
+
+  var errorMessage = addNotification(input);
+  input.after(createErrorElement(errorMessage));
 }
 
-$(".signup-input").on("input", function() {
-  var $input = $(this);
-  clearInputError($input);
+function showInvalidEmailError(input) {
+  input.setAttribute("placeholder", input.value);
+  input.classList.add("empty-signup-input");
 
-  if(isFieldEmpty($input.val()))
-    showEmptyFieldError($input);
-  else if($input.attr("type") === "email" && !$input[0].checkValidity()) 
-    showInvalidEmailError($input);
+  var errorMessage = "Looks like this is not an email";
+  input.after(createErrorElement(errorMessage));
+}
+
+/* Event handlers */ 
+document.querySelectorAll(".signup-input").forEach(function(input) {
+  input.addEventListener("input", function() {
+    clearInputError(input);
+
+    if (isFieldEmpty(input.value)) {
+      showEmptyFieldError(input);
+    } else if (input.type === "email" && !input.checkValidity()) {
+      showInvalidEmailError(input);
+    }
+  });
 });
 
-$(".signup-button").on("click", function() {
-  $(".signup-input").each(function() {
-    var $input = $(this);
-    clearInputError($input);
+document.querySelector(".signup-button").addEventListener("click", function() {
+  document.querySelectorAll(".signup-input").forEach(function(input) {
+    clearInputError(input);
 
-    if(isFieldEmpty($input.val()))
-      showEmptyFieldError($input);
-    else if($input.attr("type") === "email" && !$input[0].checkValidity())
-     showInvalidEmailError($input);
-
+    if (isFieldEmpty(input.value)) {
+      showEmptyFieldError(input);
+    } else if (input.type === "email" && !input.checkValidity()) {
+      showInvalidEmailError(input);
+    }
   });
 });
